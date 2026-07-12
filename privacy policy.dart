c39701e6b5ca92b8e3a8e8f021b0ef09c51eea67
Devakshi19 <panchaldevakshi1909@gmail.com>
2026-07-12 18:15:33 +0530

    Add files via upload

---
+++ privacy policy.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PrivacyPage extends StatelessWidget {
+  const PrivacyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PRIVACY POLICY",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.indigoAccent,
+        foregroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to Privacy policy Page",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
