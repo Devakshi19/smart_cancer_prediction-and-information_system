6350547a6bb99713c28a79648047aad3c68edff5
Devakshi19 <panchaldevakshi1909@gmail.com>
2026-07-21 22:32:40 +0530

    Remove duplicate exports in screens.dart

--- screens.dart
+++ screens.dart
@@ -3,5 +3,4 @@ export 'screens/homepage.dart';
 export 'screens/login.dart';
 export 'screens/Splashscreen.dart';
 export 'screens/Help.dart';
-export 'screens/privacy policy.dart';
 export 'screens/Terms&condition.dart';
